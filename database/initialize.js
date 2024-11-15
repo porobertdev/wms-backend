8ab6e95411f531cd4d452c403c429608e2b4393d
Robert P <porobertdev@gmail.com>
2024-11-15 07:24:11 +0200

    db: Initialize schema

---
+++ database/initialize.js
@@ -0,0 +1,39 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const pool = require('./pool');
+
+const schemaOrder = [
+    'Warehouse',
+    'Person',
+    'Employee',
+    'Role',
+    'User',
+    'Customer',
+    'Product',
+    'Inventory',
+    'Order',
+    'Car',
+    'Driver',
+];
+
+const initDb = async () => {
+    console.info('[DB] - Creating Schema...');
+
+    try {
+        for (const schema of schemaOrder) {
+            console.log('🚀 ~ initDb ~ s:', schema);
+            const filePath = path.join(
+                './',
+                'database',
+                'models',
+                `${schema}.sql`
+            );
+            const sql = fs.readFileSync(filePath, 'utf8');
+            await pool.query(sql);
+        }
+    } catch (err) {
+        console.error('Error creating db schema', err);
+    }
+};
+
+module.exports = initDb;
