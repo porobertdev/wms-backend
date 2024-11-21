e893be1ddea123fcdd3cd39e99e6da4a6769e3fd
Robert P <porobertdev@gmail.com>
2024-11-21 20:35:54 +0200

    db: Add more queries
    * Mainly delete and update.
    * Also, grouped some queries, like:
    * e.g. db.role.permission.insert

--- database/queries/Inventory.js
+++ database/queries/Inventory.js
@@ -23,7 +23,55 @@ const insertTransaction = async (data) => {
     );
 };
 
-const insertProductInventory = async (data) => {
+/**
+ * Delete a transaction
+ * @param {number} roleID - user_role table
+ * @param {number} permissionID - permissions table
+ */
+const deleteTransaction = async (transactionID) => {
+    await pool.query(
+        `
+        DELETE FROM inventory_transaction
+        WHERE id = ${transactionID}
+        `
+    );
+};
+
+/**
+ * Return all transactions made on a product
+ * @param {number} productID - product ID
+ */
+const getProductTransactions = async (productID) => {
+    const results = await pool.query(
+        `
+        SELECT * FROM inventory_transaction
+        WHERE product_id = ${productID}
+        `
+    );
+
+    return results.rows;
+};
+
+/**
+ * Return all transactions made by a specific user
+ * @param {number} userID - user ID
+ */
+const getUserTransactions = async (userID) => {
+    const results = await pool.query(
+        `
+        SELECT * FROM inventory_transaction
+        WHERE user_id = ${userID}
+        `
+    );
+
+    return results.rows;
+};
+
+/**
+ * Add a product to a location
+ * @param {object} data - product_id, warehouse_id, location_id, quantity
+ */
+const insertProduct = async (data) => {
     const { product_id, warehouse_id, location_id, quantity } = data;
 
     await pool.query(
@@ -36,7 +84,81 @@ const insertProductInventory = async (data) => {
     );
 };
 
+/**
+ * Get all locations where a product is stored for a specific warehouse
+ * @param {number} productID - product ID
+ * @param {number} warehouseID - warehouse ID
+ */
+const getProductLocation = async (productID, warehouseID) => {
+    const results = await pool.query(
+        `
+        SELECT * FROM product_inventory
+        WHERE product_id = ${productID} AND warehouse_id = ${warehouseID}
+        `
+    );
+
+    return results.rows;
+};
+
+/**
+ * Change the location of a product. NO need to mention location_id and warehouse_id since we target the row id
+ * @param {number} id - the row's id from product_inventory that holds the product
+ * @param {number} warehouseID - warehouse ID
+ * @param {number} locationID - the new location
+ */
+const updateProductLocation = async (id, warehouseID, locationID) => {
+    await pool.query(
+        `
+        UPDATE product_inventory
+        SET location_id = ${locationID}
+        WHERE id = ${id} AND warehouse_id = ${warehouseID}
+        `
+    );
+};
+
+/**
+ * Update product quantity for a location in a warehouse. NO need to mention location_id and warehouse_id since we target the row id
+ * @param {number} id - the row's id from product_inventory that holds the product
+ * @param {number} locationID - the new location
+ */
+const updateProductQuantity = async (id, quantity) => {
+    await pool.query(
+        `
+        UPDATE product_inventory
+        SET quantity = ${quantity}
+        WHERE id = ${id}
+        `
+    );
+};
+
+/**
+ * Get a list of all products from a specific location and warehouse aka 'stoc locatie' in Autonet
+ * @param {number} productID - product ID
+ * @param {number} warehouseID - warehouse ID
+ */
+const getProductList = async (warehouseID, locationID) => {
+    const results = await pool.query(
+        `
+        SELECT product_id FROM product_inventory
+        WHERE warehouse_id = ${warehouseID} AND location_id = ${locationID}
+        `
+    );
+
+    return results.rows;
+};
+
 module.exports = {
-    insertTransaction,
-    insertProductInventory,
+    product: {
+        insert: insertProduct,
+        getLocation: getProductLocation,
+        getList: getProductList,
+        updateLocation: updateProductLocation,
+        updateQuantity: updateProductQuantity,
+    },
+    transaction: {
+        insert: insertTransaction,
+        delete: deleteTransaction,
+        getProductTransactions,
+        getUserTransactions,
+    },
 };
--- database/queries/Role.js
+++ database/queries/Role.js
@@ -11,6 +11,19 @@ const insertPermission = async (name) => {
     );
 };
 
+/**
+ * Delete a permission
+ * @param {string} name - name of the permission
+ */
+const deletePermission = async (name) => {
+    await pool.query(
+        `
+        DELETE FROM permission
+        WHERE name = ${name}
+        `
+    );
+};
+
 const insertUserRole = async (name) => {
     await pool.query(
         `
@@ -22,21 +35,58 @@ const insertUserRole = async (name) => {
     );
 };
 
-const insertRolePermission = async (data) => {
-    const { role_id, permission_id } = data;
+/**
+ * Delete a role
+ * @param {string} name - name of the role
+ */
+const deleteRole = async (name) => {
+    await pool.query(
+        `
+        DELETE FROM user_role
+        WHERE name = ${name}
+        `
+    );
+};
 
+/**
+ * Add a permission to a role
+ * @param {number} roleID - user_role table
+ * @param {number} permissionID - permissions table
+ */
+const insertRolePermission = async (roleID, permissionID) => {
     await pool.query(
         `
         INSERT INTO role_permission
         (role_id, permission_id)
         VALUES ($1, $2)
         `,
-        [role_id, permission_id]
+        [roleID, permissionID]
+    );
+};
+
+/**
+ * Delete a permission of a user role
+ * @param {number} roleID - user_role table
+ * @param {number} permissionID - permissions table
+ */
+const deleteRolePermission = async (roleID, permissionID) => {
+    await pool.query(
+        `
+        DELETE FROM role_permission
+        WHERE role_id = ${roleID} AND permission_id = ${permissionID}
+        `
     );
 };
 
 module.exports = {
-    insertPermission,
-    insertUserRole,
-    insertRolePermission,
+    permission: {
+        insert: insertPermission,
+        delete: deletePermission,
+    },
+    rolePermission: {
+        insert: insertRolePermission,
+        delete: deleteRolePermission,
+    },
+    insert: insertUserRole,
+    delete: deleteRole,
 };
