deab1c8a62b1c543dff2a77f0b7ed4fdb1abf4dc
Robert P <porobertdev@gmail.com>
2024-11-29 07:34:54 +0200

    db: Create a table for customer users
    * I think it's better to store it in a different
    table since the auth process is different than
    employees of the warehouse, and the credentials
    are a bit different.

--- database/models/User.sql
+++ database/models/User.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS users CASCADE;
 
+-- users for employees
 CREATE TABLE IF NOT EXISTS users(
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     employee_id INT REFERENCES employee(id) ON DELETE CASCADE UNIQUE NOT NULL,
@@ -7,5 +8,15 @@ CREATE TABLE IF NOT EXISTS users(
     username VARCHAR(5) UNIQUE NOT NULL,
     password VARCHAR(4) NOT NULL,
     created_at DATE DEFAULT CURRENT_DATE
-)
+);
 
+-- users for customers on the e-commerce platform
+CREATE TABLE IF NOT EXISTS c_users(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(20) UNIQUE NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    role_id INT REFERENCES user_role(id) ON DELETE SET NULL,
+    isverified BOOLEAN DEFAULT FALSE,
+    created_at DATE DEFAULT CURRENT_DATE
+);
