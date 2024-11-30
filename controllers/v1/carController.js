da845bc0609b7f4f0c84aeea6d29ac38a6321d89
Robert P <porobertdev@gmail.com>
2024-11-30 13:52:19 +0200

    api: Fix typo in `carController`

--- controllers/v1/carController.js
+++ controllers/v1/carController.js
@@ -1,7 +1,7 @@
 const crudController = require('./crudController');
 
 const carController = crudController('car');
-const carIssueController = crudController('order_item');
+const carIssueController = crudController('car_issue');
 
 // TODO: break up the foreign tables since it needs different name
 module.exports = {
