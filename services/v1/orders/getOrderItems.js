89f706509967faa41eef6f74968953eacaf28873
Robert P <porobertdev@gmail.com>
2024-12-07 21:55:25 +0200

    services: Filter results from `getOrderItems`
    * Now we have unique items in the resulted array,
    but with a locations property that contains an arr
    of all locations where the item (product) is stored.

--- services/v1/orders/getOrderItems.js
+++ services/v1/orders/getOrderItems.js
@@ -8,7 +8,7 @@ const getOrderItems = async (id) => {
 
     try {
         // Get all product info required for creating Picking Lists
-        const results = await knex('order_item')
+        const products = await knex('order_item')
             .innerJoin('product', function () {
                 this.on('order_item.product_id', '=', 'product.id').andOn(
                     'order_item.order_id',
@@ -22,17 +22,59 @@ const getOrderItems = async (id) => {
                 'order_item.product_id',
                 'product.name',
                 'product.sku',
+                'product.counter_type',
                 'order_item.quantity as order_quantity',
                 'pi.location_id',
                 'bl.location_code',
                 'pi.quantity as location_quantity'
             );
 
-        if (results.length === 0) {
+        if (products.length === 0) {
             return { message: `Order ID ${id} was not found` };
         }
 
-        return results;
+        /*
+        The products array can contain multiple objects
+        representing the same product_id, but having different locations.
+
+        Filter the products so that we end up with an array of
+        unique objects, where each object contains a `locations` array.
+        */
+        const filtered = [];
+
+        products.forEach((item) => {
+            const index = filtered.findIndex(
+                (p) => p.product_id === item.product_id
+            );
+
+            if (index === -1) {
+                filtered.push({
+                    product_id: item.product_id,
+                    name: item.name,
+                    sku: item.sku,
+                    counter_type: item.counter_type,
+                    order_quantity: item.order_quantity,
+                    locations: [
+                        {
+                            location_id: item.location_id,
+                            location_code: item.location_code,
+                            location_quantity: item.location_quantity,
+                        },
+                    ],
+                });
+            } else {
+                filtered[index].locations.push({
+                    location_id: item.location_id,
+                    location_code: item.location_code,
+                    location_quantity: item.location_quantity,
+                });
+            }
+        });
+
+        return {
+            order_id: id,
+            products: filtered,
+        };
     } catch (err) {
         console.error(err);
     }
